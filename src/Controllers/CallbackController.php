3f9716b05e6ae47b914db82d01d3b4f5be1edac6
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-06-02 00:44:05 +0530

    Update CallbackController.php

--- src/Controllers/CallbackController.php
+++ src/Controllers/CallbackController.php
@@ -306,7 +306,7 @@ class CallbackController extends Controller
                 {
                         if ($nnTransactionHistory->order_paid_amount < $nnTransactionHistory->order_total_amount)
                         {
-                        $this->getLogger(__METHOD__)->error('level 2', $this->aryCaptureParams['payment_type']);
+                        $this->getLogger(__METHOD__)->error('history', $nnTransactionHistory);
                             
                             $callbackComments = sprintf($this->paymentHelper->getTranslatedText('callback_initial_execution',$orderLanguage), $this->aryCaptureParams['shop_tid'], ($this->aryCaptureParams['amount'] / 100), $this->aryCaptureParams['currency'], date('Y-m-d H:i:s'), $this->aryCaptureParams['tid'] ).'</br>';
                             if($nnTransactionHistory->order_total_amount <= ($nnTransactionHistory->order_paid_amount + $this->aryCaptureParams['amount']))
