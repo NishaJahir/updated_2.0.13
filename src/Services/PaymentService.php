0aa8348e7e2ed1a72769450cdc5ccb288fc86012
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-06-01 12:43:31 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -197,6 +197,7 @@ class PaymentService
      */
     public function executePayment($requestData, $callbackfailure = false)
     {
+	    $this->getLogger(__METHOD__)->error('executepayment', $requestData);
         try {
             if(!$callbackfailure &&  in_array($requestData['status'], ['100', '90'])) {
                 if(in_array($requestData['tid_status'], ['86', '90'])) {
